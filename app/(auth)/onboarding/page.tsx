cf43de133872994c064fa49f5a6bcb770738cf66
Jacob Berry <jacobtberry99@gmail.com>
2023-09-22 16:29:03 -0400

    Second commit

--- app/(auth)/onboarding/page.tsx
+++ app/(auth)/onboarding/page.tsx
@@ -1,24 +1,28 @@
 import AccountProfile from "@/components/forms/AccountProfile";
+import { fetchUser } from "@/lib/actions/user.actions";
 import { currentUser } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 const Page = async () => {
 	const user = await currentUser();
 
-	const userInfo = {
-		id: "",
-		username: "",
-		name: "",
-		bio: "",
-		image: "",
-	};
+	if (!user) {
+		return null;
+	}
+
+	const userInfo = await fetchUser(user.id);
+
+	if (userInfo?.onboarded) {
+		// redirect("/");
+	}
 
 	const userData = {
 		id: user?.id,
-		objectId: userInfo?.id,
-		username: userInfo?.username || user?.username,
-		name: userInfo?.name || user?.firstName || " ",
-		bio: userInfo?.bio || " ",
-		image: userInfo?.image || user?.imageUrl,
+		objectId: userInfo && userInfo?.id,
+		username: userInfo ? userInfo?.username : user?.username,
+		name: userInfo ? userInfo?.name : user?.firstName || " ",
+		bio: userInfo ? userInfo?.bio : " ",
+		image: userInfo ? userInfo?.image : user?.imageUrl,
 	};
 	return (
 		<main className="mx-auto flex max-w-3xl flex-col justify-start px-10 py-20">
--- components/shared/ThreadsTab.tsx
+++ components/shared/ThreadsTab.tsx
@@ -1,7 +1,34 @@
 import { fetchUserPosts } from "@/lib/actions/user.actions";
 import { redirect } from "next/navigation";
 import ThreadCard from "../cards/ThreadCard";
+import { fetchCommunityPosts } from "@/lib/actions/community.actions";
 
+interface Result {
+	name: string;
+	image: string;
+	id: string;
+	threads: {
+		_id: string;
+		text: string;
+		parentId: string | null;
+		author: {
+			name: string;
+			image: string;
+			id: string;
+		};
+		community: {
+			id: string;
+			name: string;
+			image: string;
+		} | null;
+		createdAt: string;
+		children: {
+			author: {
+				image: string;
+			};
+		}[];
+	}[];
+}
 interface Props {
 	currentUserId: string;
 	accountId: string;
@@ -9,15 +36,18 @@ interface Props {
 }
 
 const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
-	let result = await fetchUserPosts(accountId);
+	let result: Result;
+
+	if (accountType === "Community") {
+		result = await fetchCommunityPosts(accountId);
+	} else {
+		result = await fetchUserPosts(accountId);
+	}
 
 	if (!result) {
 		redirect("/");
 	}
 
-	if (result) {
-		console.log("test");
-	}
 	return (
 		<section className="mt-9 flex flex-col gap-10">
 			{result.threads.map((thread: any) => (
@@ -35,8 +65,12 @@ const ThreadsTab = async ({ currentUserId, accountId, accountType }: Props) => {
 									image: thread.author.image,
 									id: thread.author.id,
 							  }
-					} //todo:
-					community={thread.community} //todo:
+					}
+					community={
+						accountType === "Community"
+							? { name: result.name, id: result.id, image: result.image }
+							: thread.community
+					}
 					createdAt={thread.createdAt}
 					comments={thread.comments}
 				/>
